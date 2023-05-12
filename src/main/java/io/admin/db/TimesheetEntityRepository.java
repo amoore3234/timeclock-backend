148ce9d49bc57ad8d4743c64f1b6264c9541396c
Anthony Moore <akmoore@cpp.edu>
2023-05-12 07:14:24 -0700

    Implement repository class

---
+++ src/main/java/io/admin/db/TimesheetEntityRepository.java
@@ -0,0 +1,15 @@
+package io.admin.db;
+
+import io.admin.core.TimesheetEntity;
+import org.hibernate.SessionFactory;
+
+/**
+ * Initializes a session factory for the TimesheetEntity repository class.
+ */
+public class TimesheetEntityRepository extends AbstractRepository<TimesheetEntity> {
+
+  protected TimesheetEntityRepository(SessionFactory sessionFactory) {
+    super(sessionFactory);
+  }
+
+}
